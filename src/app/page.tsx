ee4ace046bc069e46840b4c9b0bdce72fd621384
mangming2 <leejiho35@gmail.com>
2023-10-16 02:51:44 +0900

    Update : publish main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,14 @@ import React, { useState } from "react";
 
 import Confetti from "react-confetti";
 
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 import { ButtonSmall } from "./components/buttons/button-small";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import ErrorPage from "./components/error";
+import MainPageImage1 from "public/main-page-1.png";
+import MainPageImageLeft from "public/main-page-left.png";
+import MainPageImageRight from "public/main-page-right.png";
+import Image from "next/image";
 
 export default function App() {
   const {
@@ -35,44 +39,67 @@ export default function App() {
         <ErrorPage />
       ) : (
         <>
-          <ButtonSmall
-            text="Get your Wallet"
-            isLoading={isConnecting || isConnected}
-            onClick={connect}
-          />
-          <Hidiv>HI</Hidiv>
-          <TitleDiv>this is Title</TitleDiv>
-
-          {/* {transactionSuccess && (
-          <Confetti width={windowWidth} height={windowHeight} />
-        )} */}
-
-          <ConnectWallet
-            isConnected={isConnected}
-            isConnecting={isConnecting}
-            isNfcConnecting={isNfcConnecting}
-            handleNfcReading={handleNfcReading}
-            connect={connect}
-            connectionError={connectionError}
-            wallet={wallet!}
-          />
-
-          {/* {isConnected && (
-                <Transaction
-                  transactionSuccess={transactionSuccess}
-                  setTransactionSuccess={setTransactionSuccess}
-                />
-              )} */}
+          <LogoBox>
+            <LogoTitle>NEBULA</LogoTitle>
+          </LogoBox>
+          <ImgBox>
+            <MainImageLeft src={MainPageImageLeft} alt="main-page-image-left" />
+            <MainImage1 src={MainPageImage1} alt="main-page-image-1" />
+            <MainImageRight
+              src={MainPageImageRight}
+              alt="main-page-image-right"
+            />
+          </ImgBox>
+          <TextBox>
+            Create a Wallet Easily and Safely with your Credit Card
+          </TextBox>
+          <ButtonBox>
+            <ButtonSmall
+              text="Get Started"
+              isLoading={isConnecting || isNfcConnecting}
+              onClick={handleNfcReading}
+            />
+            <ButtonSmall
+              text="I Have an Wallet"
+              isLoading={isConnecting || isNfcConnecting}
+              onClick={connect}
+            />
+          </ButtonBox>
         </>
       )}
     </>
   );
 }
 
-const Hidiv = tw.div`
-  bg-red-500 font-sb-12
+const LogoBox = tw.div`
+  w-full h-100 flex-center
 `;
 
-const TitleDiv = tw.div`
-  bg-gray8 font-b-40
+const LogoTitle = tw.div`
+  font-b-40 text-white
+`;
+
+const ImgBox = tw.div`
+  w-full flex-center relative
+  max-h-600
+`;
+
+const MainImageLeft = tw(Image)`
+  absolute top-0 left-0 w-100
+`;
+
+const MainImage1 = tw(Image)`
+  flex max-w-400
+`;
+
+const MainImageRight = tw(Image)`
+  absolute bottom-0 right-0 w-100
+`;
+
+const TextBox = tw.div`
+  flex-center font-r-14 text-white text-center
+`;
+
+const ButtonBox = tw.div`
+  flex-col flex-center gap-15 m-26
 `;
