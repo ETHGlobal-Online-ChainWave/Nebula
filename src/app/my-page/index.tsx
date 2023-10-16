34675855ed834b7c672b0d8c4248ad8813fdbd96
mangming2 <leejiho35@gmail.com>
2023-10-16 18:29:27 +0900

    Update : page summary

--- src/app/my-page/index.tsx
+++ src/app/my-page/index.tsx
@@ -36,8 +36,12 @@ export const MyPage = ({ isSuccess }: Props) => {
     <>
       <Wrapper>
         <UserSummaryBox>
-          <Ellipse src={EllipseImage} alt="credit-card-image" />
+          <ServiceTitle>NEBULA</ServiceTitle>
+          <UserBalance>$1,235 USD</UserBalance>
+          <AccountName>Account Name</AccountName>
+          <AccountNumber>0x7F3...9901</AccountNumber>
         </UserSummaryBox>
+        <Ellipse src={EllipseImage} alt="credit-card-image" />
 
         <LottieWrapper ref={warpperRef} />
       </Wrapper>
@@ -54,15 +58,27 @@ const Wrapper = tw.div`
 `;
 
 const UserSummaryBox = tw.div`
-    flex-center flex-col
+    flex-center flex-col relative gap-20 pt-100
     `;
 
 const Ellipse = tw(Image)`
-    max-w-270
+    absolute top-0 w-350
     `;
 
-const CardImage = tw(Image)`
-    max-w-270
+const ServiceTitle = tw.div`
+    font-sb-16 text-white
+    `;
+
+const UserBalance = tw.div`
+    font-b-32 text-white
+    `;
+
+const AccountName = tw.div`
+    font-sb-12 text-white
+    `;
+
+const AccountNumber = tw.div`
+    font-sb-12 text-white
     `;
 
 const LottieWrapper = tw.div`
